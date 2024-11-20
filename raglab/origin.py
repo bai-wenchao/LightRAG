f321d8c9bf3a8880b4de0d858fecf4d5caefb667
bai-wenchao <wenchao.bai.chn@gmail.com>
2024-11-20 14:50:32 +0800

    Updated origin.py

--- raglab/origin.py
+++ raglab/origin.py
@@ -1,6 +1,12 @@
-from raglab import RAGManager
+from raglab import ConfigManager, GraphVis, RAGManager
 
 if __name__ == "__main__":
-    rag_manager = RAGManager("config/origin.yaml")
+    config_manager = ConfigManager("config/origin.yaml")
+
+    rag_manager = RAGManager(config_manager=config_manager)
+    graph_vis = GraphVis(config_manager=config_manager)
+    
     rag_manager.doc2kg()
     rag_manager.first_item_query()
+
+    graph_vis.convert()
